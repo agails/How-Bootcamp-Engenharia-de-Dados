72eae1ce14aa55b896beff3d7f6aec7fd6cd0f3c
Agail Sanchez <email@agail.com.br>
2021-06-20 11:44:52 -0300

    Update SELECT INTO.sql

--- Module 2/sql/SELECT INTO.sql
+++ Module 2/sql/SELECT INTO.sql
@@ -1,42 +1,72 @@
 select
-    TBB."date",
-    TBB."rank",
-    TBB.song,
-    TBB.artist,
-    TBB."last-week",
-    TBB."peak-rank",
-    TBB."weeks-on-board"
+       TBB."date"
+       , TBB."rank"
+       , TBB.song
+       , TBB.artist
+       , TBB."last-week"
+       , TBB."peak-rank"
+       , TBB."weeks-on-board"
 from
-    "Billboard" as TBB
+       "Billboard" as TBB
 limit 10;
 
-select max(TBB."date") as max_date from "Billboard" as TBB limit 10; 
-
-select max(TBB."date") as max_date from "Billboard" as TBB;
-
-select TBB."date" as max_date from "Billboard" as TBB order by "date" desc
-limit 1 ;
+select
+       count(TBB.date) as days
+from
+       "Billboard" as TBB;
 
 select
-    TBB."date",
-    TBB."rank",
-    TBB.song,
-    TBB.artist,
-    TBB."last-week",
-    TBB."peak-rank",
-    TBB."weeks-on-board"
+       max(TBB."date") as max_date
 from
-    "Billboard" as TBB
-where TBB."date" = '2021-03-13';
+       "Billboard" as TBB;
 
 select
-    TBB."date",
-    TBB."rank",
-    TBB.song,
-    TBB.artist,
-    TBB."last-week",
-    TBB."peak-rank",
-    TBB."weeks-on-board"
+       count(*) as lines
 from
-    "Billboard" as TBB
-where TBB."date" = '2021-03-13' and TBB."rank" <= 10;
+       "Billboard" as TBB;
+
+select
+       TBB."date"
+       , TBB."rank"
+       , TBB.song
+       , TBB.artist
+       , TBB."last-week"
+       , TBB."peak-rank"
+       , TBB."weeks-on-board"
+from
+       "Billboard" as TBB
+where
+       TBB."date" = '2021-03-13'
+
+order by 2;
+
+select
+       TBB."date"
+       , TBB."rank"
+       , TBB.song
+       , TBB.artist
+       , TBB."last-week"
+       , TBB."peak-rank"
+       , TBB."weeks-on-board"
+from
+       "Billboard" as TBB
+where
+       TBB."date" = '2021-03-13'
+       and TBB."rank" <= 10;
+       
+select
+       distinct TBB.artist
+       , TBB.song
+       , MAX(TBB."weeks-on-board") as max_weeks
+from
+       "Billboard" as TBB
+where
+       TBB.artist like 'Metallica'
+group by
+       TBB.song
+       , TBB.artist
+order by
+       3 desc;
+
+
+
